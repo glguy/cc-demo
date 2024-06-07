4239212994f3d35fd8cbd0bad2099abb76904045
Eric Mertens <emertens@gmail.com>
2024-06-06 21:17:13 -0700

    simplify

--- app/Coroutine.hs
+++ app/Coroutine.hs
@@ -8,7 +8,7 @@ module Coroutine (
     isDead,
 ) where
 
-import GHC.Prim ( PromptTag#, newPromptTag#, prompt#, control0# )
+import GHC.Prim ( PromptTag#, newPromptTag#, prompt#, control0#, State#, RealWorld )
 import GHC.IO ( IO(IO), unIO )
 import Data.IORef ( IORef, newIORef, readIORef, writeIORef )
 
@@ -16,8 +16,8 @@ data Coroutine i o = Coroutine (PromptTag# o) (IORef (Status i o))
 
 data Status i o
     = Running
-    | Suspended (i -> IO o)
     | Dead
+    | Suspended ((State# RealWorld -> (# State# RealWorld, i #)) -> State# RealWorld -> (# State# RealWorld, o #))
 
 isDead :: Coroutine i o -> IO Bool
 isDead (Coroutine _ r) =
@@ -29,14 +29,17 @@ isDead (Coroutine _ r) =
 
 new :: (Coroutine i o -> i -> IO o) -> IO (Coroutine i o)
 new k =
- do r <- newIORef Dead -- placeholder
+ do r <- newIORef undefined -- placeholder
     IO \s0# ->
         case newPromptTag# s0# of
             (# s1# , p# #) ->
                 case Coroutine p# r of
                     c ->
                         unIO
-                        (c <$ writeIORef r (Suspended (\i -> IO (prompt# p# (unIO (k c i <* writeIORef r Dead)))))) s1#
+                        (c <$ writeIORef r (Suspended \getI# sa# ->
+                             case getI# sa# of
+                                (# sb# , i #) ->
+                                    unIO (k c i) sb#)) s1#
 
 yield :: Coroutine i o -> o -> IO i
 yield (Coroutine p# r) o = IO $
@@ -46,16 +49,14 @@ yield (Coroutine p# r) o = IO $
             Dead -> fail "yield on dead coroutine"
             Suspended{} -> fail "yield on suspended coroutine"
             Running ->
-                o <$ writeIORef r (Suspended (\i -> IO (prompt# p# (k (# , i #)))))
-            
+                o <$ writeIORef r (Suspended k)
 
 resume :: Coroutine i o -> i -> IO o
-resume (Coroutine _ r) i =
+resume (Coroutine p# r) i =
  do mb <- readIORef r
     case mb of
         Dead -> fail "resume on dead coroutine"
         Running -> fail "resume on running coroutine"
-        Suspended k ->
+        Suspended k# ->
          do writeIORef r Running
-            k i
-        
+            IO (prompt# p# (k# (# , i #)))
